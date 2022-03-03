64b69627a04acccadf74af15974eafd3d7466ffd
Bruno <bruno199723@gmail.com>
2022-03-03 18:35:00 -0300

    Added create, show, all
    Co-authored-by: AngelPiva <angelpiva6@gmail.com>

--- console.py
+++ console.py
@@ -2,10 +2,13 @@
 Console - entry point of the command interpreter
 """
 import cmd
+from models import storage
+from models.base_model import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
     prompt = '(hbnb) '
+    cls_list = ["BaseModel"]
 
     def do_quit(self, line):
         """Quit command to exit the program"""
@@ -14,6 +17,63 @@ class HBNBCommand(cmd.Cmd):
     def do_EOF(self, line):
         """End of File"""
         return True
+
+    def do_create(self, line):
+        if line == "":
+            print("** class name missing **")
+            return
+        if line != "BaseModel":
+            print("** class doesn't exist **")
+            return
+
+        obj = BaseModel()
+        print(f"{obj.id}")
+
+    def do_show(self, line):
+        if line == "":
+            print("** class name missing **")
+            return
+
+        l = line.split()
+        if len(l) == 1:
+            l.append("")
+
+        for cls in self.cls_list:
+            if cls == l[0]:
+                all_objs = storage.all()
+                for k in all_objs.keys():
+                    if all_objs[k].id == l[1]:
+                        print(all_objs[k])
+                        return
+                print("** instance id missing **")
+                return
+
+        print("** class doesn't exist **")
+
+    def do_destroy(self, line):
+        pass
+    def do_all(self, line):
+        split_line = line.split()
+        count = 1
+        if line != "":
+            for cls in self.cls_list:
+                if cls != split_line[0]:
+                    count = 0
+                else:
+                    count = 1
+        if count == 0:
+            print("** class doesn't exist **")
+            return
+        else:
+            all_objs = storage.all()
+            for obj_id in all_objs.keys():
+                obj = all_objs[obj_id]
+                print(obj)
+            return
+
+    def do_update(self, line):
+        pass
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
